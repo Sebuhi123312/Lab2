5db1e31ba7e21c8e857119c160aee0e1aeb05665
Sebuhi123312 <sebuhicemili@gmail.com>
2024-03-05 14:11:54 +0100

    lab 2

--- src/main/java/Hello.java
+++
@@ -1,9 +0,0 @@
-package pl.pp;
-
-public class myFirstApp {
-
-        public static void main(String[] arg) {
-
-            System.out.println("Hello Basics of Programming!");
-        }
-}
---
+++ src/main/mySecondApp.java/pl/pp/mySecondApp.java
@@ -0,0 +1,33 @@
+package pl.pp;
+
+import java.util.Scanner;
+
+public class mySecondApp {
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        int doubleX = x * 2;
+        int xSquared = x * x;
+
+        System.out.println("x = " + x);
+        System.out.println("Double of x = " + doubleX);
+        System.out.println("x squared = " + xSquared);
+
+    }
+}
+
+/*
+    Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Please enter your name:");
+                String forename = scanner.nextLine();
+
+                System.out.println("Please enter your surname:");
+                String surname = scanner.nextLine();
+
+                scanner.close();
+
+                System.out.println("Welcome " + forename + " " + surname);
+                */
